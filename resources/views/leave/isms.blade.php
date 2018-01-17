63adc4a42678cfc9a3dbc960addf3601aceafb9f
a57785778 <a57785778@yahoo.com.tw>
2018-01-17 10:03:56 +0800

    20180117

--- resources/views/leave/isms.blade.php
+++ resources/views/leave/isms.blade.php
@@ -27,121 +27,118 @@
                 <a class="nav-link" href="#">Home</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#" data-toggle="modal" data-target="#exampleModalCenter">
-                <i class="fa fa-user-circle-o fa-lg" aria-hidden="true"></i>
-                申請</a>
+                <a class="nav-link" href="#" data-toggle="modal" data-target="#exampleModalCenter">申請</a>
                 <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-                        <div class="modal-dialog" role="document">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalLongTitle">使用者登入</h5>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
-                            </div>
-                            <div class="modal-body">
-                                <div>
-                                    <span>帳號:</span>
-                                    <input type="textbox">
-                                </div>
-                                <div class="py-2">
-                                    <span>密碼:</span>
-                                    <input type="textbox">
-                                </div>
-                                <div>
-                                    <a href="#">忘記密碼</a>
-                                </div>
-                                <hr width="100%">
-                                <div>
-                                    <span>沒有帳密?</span>
-                                    <a href="#">註冊</a>
-                                </div>
-                            </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-dismiss="modal">取消</button>
-                                <button type="button" class="btn btn-primary">登入</button>
-                            </div>
-                        </div>
-                        </div>
+                <div class="modal-dialog modal-dialog-centered" role="document">
+                    <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLongTitle">使用者登入</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                        </button>
                     </div>
+                    <div class="modal-body">
+                            <div>
+                                <span>帳號:</span>
+                                <input type="text" >
+                            </div>
+                            <div class="py-2">
+                                <span>密碼:</span>
+                                <input type="password">
+                            </div>
+                            <div>
+                                <a href="#">忘記密碼</a>
+                            </div>
+                            <hr width="100%">
+                            <div>
+                                <span>沒有帳密?</span>
+                                <a  href="#" data-toggle="modal" data-target="#exampleModal">註冊</a>
+                                    
+                            </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">取消</button>
+                        <button type="button" class="btn btn-primary">登入</button>
+                    </div>
+                    </div>
+                </div>
+                </div>
             </li>
             <li class="nav-item">
-                    <a class="nav-link" href="#" data-toggle="modal" data-target="#exampleModalCenter">
-                <i class="fa fa-user-circle-o fa-lg" aria-hidden="true"></i>
-                待辦事項</a>
-                <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-                        <div class="modal-dialog" role="document">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalLongTitle">使用者登入</h5>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
-                            </div>
-                            <div class="modal-body">
-                                <div>
-                                    <span>帳號:</span>
-                                    <input type="textbox">
-                                </div>
-                                <div class="py-2">
-                                    <span>密碼:</span>
-                                    <input type="textbox">
-                                </div>
-                                <div>
-                                    <a href="#">忘記密碼</a>
-                                </div>
-                                <hr width="100%">
-                                <div>
-                                    <span>沒有帳密?</span>
-                                    <a href="#">註冊</a>
-                                </div>
-                            </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-dismiss="modal">取消</button>
-                                <button type="button" class="btn btn-primary">登入</button>
-                            </div>
-                        </div>
-                        </div>
+                    <a class="nav-link" href="#" data-toggle="modal" data-target="#exampleModalCenter">待辦事項</a>
+                    <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered" role="document">
+                    <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLongTitle">使用者登入</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                        </button>
                     </div>
+                    <div class="modal-body">
+                            <div>
+                                <span>帳號:</span>
+                                <input type="text" >
+                            </div>
+                            <div class="py-2">
+                                <span>密碼:</span>
+                                <input type="password">
+                            </div>
+                            <div>
+                                <a href="#">忘記密碼</a>
+                            </div>
+                            <hr width="100%">
+                            <div>
+                                <span>沒有帳密?</span>
+                                <a  href="#" data-toggle="modal" data-target="#exampleModal">註冊</a>
+                                    
+                            </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">取消</button>
+                        <button type="button" class="btn btn-primary">登入</button>
+                    </div>
+                    </div>
+                </div>
+                </div>
                 </li>
             <li class="nav-item">
-                <a class="nav-link" href="#" data-toggle="modal" data-target="#exampleModalCenter">
-                <i class="fa fa-user-circle-o fa-lg" aria-hidden="true"></i>
-                完成事項</a>
+                <a class="nav-link disabled" href="#" data-toggle="modal" data-target="#exampleModalCenter">完成事項</a>
                 <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-                        <div class="modal-dialog" role="document">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalLongTitle">使用者登入</h5>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
-                            </div>
-                            <div class="modal-body">
-                                <div>
-                                    <span>帳號:</span>
-                                    <input type="textbox">
-                                </div>
-                                <div class="py-2">
-                                    <span>密碼:</span>
-                                    <input type="textbox">
-                                </div>
-                                <div>
-                                    <a href="#">忘記密碼</a>
-                                </div>
-                                <hr width="100%">
-                                <div>
-                                    <span>沒有帳密?</span>
-                                    <a href="#">註冊</a>
-                                </div>
-                            </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-dismiss="modal">取消</button>
-                                <button type="button" class="btn btn-primary">登入</button>
-                            </div>
-                        </div>
-                        </div>
+                <div class="modal-dialog modal-dialog-centered" role="document">
+                    <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLongTitle">使用者登入</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                        </button>
                     </div>
+                    <div class="modal-body">
+                            <div>
+                                <span>帳號:</span>
+                                <input type="text" >
+                            </div>
+                            <div class="py-2">
+                                <span>密碼:</span>
+                                <input type="password">
+                            </div>
+                            <div>
+                                <a href="#">忘記密碼</a>
+                            </div>
+                            <hr width="100%">
+                            <div>
+                                <span>沒有帳密?</span>
+                                <a  href="#" data-toggle="modal" data-target="#exampleModal">註冊</a>
+                                    
+                            </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">取消</button>
+                        <button type="button" class="btn btn-primary">登入</button>
+                    </div>
+                    </div>
+                </div>
+                </div>
             </li>
             </ul>
             <div class="user">
@@ -150,39 +147,86 @@
                 <strong>登入</strong>
                 </a>
                 <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-                        <div class="modal-dialog" role="document">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalLongTitle">使用者登入</h5>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
+                <div class="modal-dialog modal-dialog-centered" role="document">
+                    <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLongTitle">使用者登入</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                            <div>
+                                <span>帳號:</span>
+                                <input type="text" >
                             </div>
-                            <div class="modal-body">
-                                <div>
-                                    <span>帳號:</span>
-                                    <input type="textbox">
-                                </div>
-                                <div class="py-2">
-                                    <span>密碼:</span>
-                                    <input type="textbox">
-                                </div>
-                                <div>
-                                    <a href="#">忘記密碼</a>
-                                </div>
-                                <hr width="100%">
-                                <div>
-                                    <span>沒有帳密?</span>
-                                    <a href="#">註冊</a>
-                                </div>
+                            <div class="py-2">
+                                <span>密碼:</span>
+                                <input type="password">
                             </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-dismiss="modal">取消</button>
-                                <button type="button" class="btn btn-primary">登入</button>
+                            <div>
+                                <a href="#">忘記密碼</a>
+                            </div>
+                            <hr width="100%">
+                            <div>
+                                <span>沒有帳密?</span>
+                                <a  href="#" data-toggle="modal" data-target="#exampleModal">註冊</a>
+                                    
+                            </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">取消</button>
+                        <button type="button" class="btn btn-primary">登入</button>
+                    </div>
+                    </div>
+                </div>
+                </div>
+                <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                    <div class="modal-dialog modal-dialog-centered" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLongTitle">註冊</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                        </div>
+                        <div class="modal-body">
+                            <div>
+                                <span>帳號:</span>
+                                <input type="text">
+                            </div>
+                            <div class="py-2">
+                                <span>密碼:</span>
+                                <input type="password">
+                            </div>
+                            <div class="py-2">
+                            <span>再次輸入密碼:</span>
+                            <input type="password">
+                            </div>
+                            <div>
+                                <span>姓名:</span>
+                                <input type="textbox">
+                            </div>
+                            <div class="py-2">
+                                <span>電子信箱:</span>
+                                <input type="textbox">
+                            </div>
+                            <div>
+                                <span>身份:</span>
+                                <select>
+                                    <option value>-選擇</option>
+                                    <option value ="stu">學生</option>
+                                    <option value ="tch">老師</option>
+                                </select>
                             </div>
                         </div>
+                        <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">取消</button>
+                        <button type="button" class="btn btn-primary">註冊</button>
                         </div>
                     </div>
+                    </div>
+                </div>
             </div>
         </div>
     </nav>
